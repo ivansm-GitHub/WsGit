329def99738d2e56b2b78f9fb71c1ba1f71ca7b8
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2023-01-19 20:00:14 +0100

    Primer commit

--- 01-PrimerProyectoGit/src/git/main/Main.java
+++ 01-PrimerProyectoGit/src/git/main/Main.java
@@ -3,7 +3,8 @@ package git.main;
 public class Main {
 
 	public static void main(String[] args) {
-		 System.out.println("Bienvenidos a nuestras versiones con GIT");
+		 
+		System.out.println("Bienvenidos a nuestras versiones con GIT");
 
 	}
 
